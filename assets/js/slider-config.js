e290bcaeeeb6cc26a37bc63221ded2ea2f4fb23e
Joe Kuczek <jkuczek@iastate.edu>
2017-08-12 16:57:04 -0500

    Updates to slider

--- assets/js/slider-config.js
+++ assets/js/slider-config.js
@@ -2,11 +2,10 @@
         Slider JavaScript Configuration 
 #########################################*/
 
-/* Main settings */
 /* Please see https://v4-alpha.getbootstrap.com/components/carousel/#options */
 
 var interval = false;       /* false = prevent autoplay, 1000 = 1 second autoplay */
 var keyboard = true;        /* whether or not keyboard input affects slider */
 var pause = 'hover';        /* if interval is set, only autoplay when mouse hover occurs */
 var ride = false;           /* autoplays the slider after user manually cycles first item */
-var wrap = true;            /* whether the carousel should cycle continuously or have hard stops. */
+var wrap = false;            /* whether the carousel should cycle continuously or have hard stops. */
